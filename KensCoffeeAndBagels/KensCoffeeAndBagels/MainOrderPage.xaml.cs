126cebf3735d5e2934111c60406e07542782e7d3
DHaug <derek.haugen@jacks.sdstate.edu>
2018-04-30 14:03:26 -0500

    Intial commit with basic app started

---
+++ KensCoffeeAndBagels/KensCoffeeAndBagels/MainOrderPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KensCoffeeAndBagels
+{
+    /// <summary>
+    /// Interaction logic for MainOrderPage.xaml
+    /// </summary>
+    public partial class MainOrderPage : Page
+    {
+        public MainOrderPage()
+        {
+            InitializeComponent();
+            
+            //This is where we handle the click events. Reference the button name and send it to a function
+            MochaButton.Click += new RoutedEventHandler(Mocha_Click);
+            FrozenDrinksButton.Click += new RoutedEventHandler(Frozen_Click);
+
+            //These Buttons have been linked to a function but not a navigation because there is no page built yet for them
+            TeaButton.Click += new RoutedEventHandler(Tea_Click);
+            BlackCoffeeButton.Click += new RoutedEventHandler(BlackCoffee_Click);
+            SpecialtyButton.Click += new RoutedEventHandler(Specialty_Click);
+            SeasonalButton.Click += new RoutedEventHandler(Seasonal_Click);
+            BlendedButton.Click += new RoutedEventHandler(Blended_Click);
+            LatteButton.Click += new RoutedEventHandler(Latte_Click);
+        }
+
+        //Navigation has been set for these
+        private void Frozen_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new FrozenDrinkPage());
+        }
+
+        private void Mocha_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MochaPage());
+        }
+
+
+        //Navigation still needs to be set once we create a page for these. 
+        private void Blended_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Latte_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Seasonal_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Specialty_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BlackCoffee_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Tea_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
